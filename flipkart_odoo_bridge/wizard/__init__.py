dcc291f1be11ea9fb1522a5c405ebf0c3540672b
priyanshicsl <priyanshi@cloudsciencelabs.com>
2025-11-27 11:02:10 +0530

    [ADD] Flipkart connector

---
+++ flipkart_odoo_bridge/wizard/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+#################################################################################
+#
+#    Copyright (c) 2019-Present Webkul Software Pvt. Ltd. (<https://webkul.com/>)
+#
+#################################################################################
+from . import import_flipkart_orders
+from . import import_flipkart_products
+from . import export_products
---
+++ wk_wizard_messages/__manifest__.py
@@ -0,0 +1,35 @@
+#############################################################################
+# Author: Webkul Software Pvt. Ltd. (<https://webkul.com/>)
+# Copyright(c): 2015-Present Webkul Software Pvt. Ltd.
+# All Rights Reserved.
+#
+# This program is copyright property of the author mentioned above.
+# You can`t redistribute it and/or modify it.
+#
+# You should have received a copy of the License along with this program.
+# If not, see <https://store.webkul.com/license.html/>
+#############################################################################
+{
+    'name': 'Message Wizard',
+    'summary': """To show messages/warnings in Odoo""",
+    'category': 'Home Kouzina',
+    'version': '1.0.0',
+    'sequence': 1,
+    'author': 'Webkul Software Pvt. Ltd.',
+    'website': 'https://store.webkul.com/Odoo.html',
+    'license':  'Other proprietary',
+    'description': """""",
+    'live_test_url': (
+        'http://odoodemo.webkul.com/'
+        '?module=wk_wizard_messages&version=16.0'
+    ),
+    'data': [
+        'security/ir.model.access.csv',
+        'wizard/wizard_message.xml',
+    ],
+
+    'images': ['static/description/Banner.png'],
+    'application': True,
+    'installable': True,
+    'pre_init_hook': 'pre_init_check',
+}
