8cd4ee393ce3cfcbe614fb1a0a09fba22ee47065
Mohd Sohail <badshahansari200@gmail.com>
2026-06-18 16:35:20 +0530

    Added the new module purchase report

---
+++ hk_purchase_report/__manifest__.py
@@ -0,0 +1,28 @@
+{
+    'name': 'HK Purchase Report',
+    'version': '18.0.1.0',
+    'summary': 'Detailed Purchase Reporting List View with Advanced Filters',
+    'description': """
+        Provides a comprehensive Purchase Reporting screen under Purchase > Reporting.
+        Shows Product, SKU, Vendor, Qty, Unit Price and many more fields.
+        Includes advanced filters and group-by options for deep analysis.
+    """,
+    'category': 'Home Kouzina',
+    'author': 'Home Kouzina',
+    'depends': ['purchase', 'purchase_stock', 'uom'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/hk_purchase_report_views.xml',
+    ],
+    'assets': {
+        'web.assets_backend': [
+            'hk_purchase_report/static/src/scss/hk_purchase_report_list.scss',
+            'hk_purchase_report/static/src/js/lot_pills_field.js',
+            'hk_purchase_report/static/src/js/lot_pills_field.xml',
+        ],
+    },
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+    'license': 'LGPL-3',
+}
