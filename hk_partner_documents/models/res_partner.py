379ad827849d2487a434512a962fef592c238e24
Priyanjali <priyanjali@cloudsciencelabs.com>
2026-06-22 12:14:49 +0530

    {UPDATE}change the type of filed cin and attachment inside doc tab of vendor

--- hk_partner_documents/models/res_partner.py
+++ hk_partner_documents/models/res_partner.py
@@ -4,6 +4,19 @@ from odoo import fields, models
 class ResPartner(models.Model):
     _inherit = "res.partner"
 
-    cin_image = fields.Image(string="CIN")
-    doc_image = fields.Image(string="Document")
+    cin_attachment_ids = fields.Many2many(
+        'ir.attachment',
+        'res_partner_cin_attachment_rel',
+        'partner_id',
+        'attachment_id',
+        string="CIN"
+    )
+
+    other_attachment_ids = fields.Many2many(
+        'ir.attachment',
+        'res_partner_other_attachment_rel',
+        'partner_id',
+        'attachment_id',
+        string="Attachments"
+    )
     gst_number = fields.Char(string="GST Number")
